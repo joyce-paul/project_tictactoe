c85e679fdc234796f5821cf5fe9d84c391064c53
joyce-paul <joyce_paul@hotmail.com>
2020-11-01 18:28:23 +0800

    Player turn code

--- tictactoe.py
+++ tictactoe.py
@@ -1,6 +1,5 @@
 # ____ WELCOME MESSAGE ____
 print ('\n' + '\033[1;33mHello, let\'s play a game of tic-tac-toe \033[0;0m ⌗ ⌗ ⌗ ! 😁 ')
-# print("\n" + "Hello, let's play a game of tic-tac-toe ⌗ ⌗ ⌗ ! 😁 ")
 print("""\nHere are the game rules:
 1. Two Players: Player X and Player O.
 2. Player X starts the game.
@@ -8,13 +7,24 @@ print("""\nHere are the game rules:
 4. Get three of your markers in a row (horizontally, vertically or diagonally) to win the game.
     """)
 
+
+# ____ ASK FOR NAMES OF PLAYER1 AND PLAYER2 ____
+playerX = input("\t" + "Enter name for Player X :" +"\t")
+playerX = playerX.capitalize() # Initial capitalize the name
+playerO = input("\n" + "\t" + "Enter name for Player O :"+"\t")
+playerO = playerO.capitalize() # Initial capitalize the name
+
+
+# ____ START GAME ____
+print("\n" + "\t" + playerX + "  vs  " + playerO + "." + " Let the game begin!")
+print("\n" + "\t" + playerX + ", " + "you are X.  " + playerO + ", " + "you are O.")
+
+
 # ____ CREATE THE BOARD ____
 square = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-# Note: Might want to add "0" and make it invisible to viewers due to indexing
-# OR : playerInput = playerInput -1
 
 
-
+# ____ DISPLAYS THE BOARD ____
 def board():
   print("\n")
   print("\t" + "\t" + square[0] + " ⎢ " + square[1] + " ⎢ " + square[2])
@@ -25,94 +35,66 @@ def board():
   print("\n")
 
 
-# ____ ASK FOR NAMES OF PLAYER1 AND PLAYER2 ____
-X = input("\t" + "Enter name for Player X :" +"\t")
-X = X.capitalize() # Initial capitalize the name
-O = input("\n" + "\t" + "Enter name for Player O :"+"\t")
-O = O.capitalize() # Initial capitalize the name
-
-players = [X, O] # Save the list of the players into a variable
-
-
-# ____ START GAME ____
-print("\n" + "\t" + X + "  vs  " + O + "." + " Let the game begin!")
-print("\n" + "\t" + X + ", " + "you are X.  " + O + ", " + "you are O.")
-
-# ____ DISPLAYS THE BOARD ____
-board()
-
-
 # ____ PLAYER TURN ____
-def addX():
-        playerInput = int(input("\t" + X + ", select a square (1-9):" + "\n"))
-        playerInput = playerInput - 1
-        square[playerInput] # Allocate playerInput to index number on list
-        if (square[playerInput] == "X") or (square[playerInput] == "O"):
-            playerInput = int(input("\t" + X + ", you selected a filled square. Try again. Select a square (1-9):" + "\n"))
-        else:
-            playerInput
-            square[playerInput] # Allocate playerInput to index number on list
-        square[playerInput] = "X"
-        board()
+turn = 0
+current_player = playerX
+current_symbol = "X"
 
-def addO():
-        playerInput = int(input("\t" + O + ", select a square (1-9):" + "\n"))
-        playerInput = playerInput - 1
-        square[playerInput] # Allocate playerInput to index number on list
-        if (square[playerInput] == "X") or (square[playerInput] == "O"):
-            playerInput = int(input("\t" + O + ", you selected a filled square. Try again. Select a square (1-9):" + "\n"))
-        else:
-            playerInput
-            square[playerInput] # Allocate playerInput to index number on list
-            square[playerInput] = "O"
-        board()
-
-
-def winGame():
+while turn < 9:
+    board()
+    input_square = input("\t" + current_player + ", select a square (1-9):" + "\n")
+    input_square_int = int(input_square)
+    square_index = input_square_int - 1
+    if (square[square_index] == "X") or (square[square_index] == "O"):
+        print(current_player + ", you selected a filled square. Try again. Select a square (1-9):")
+    square[square_index] = current_symbol
+    turn = turn + 1
+    if current_player == playerX:
+        current_player = playerO
+        current_symbol = "O"
+    else:
+        current_player = playerX
+        current_symbol = "X"
     if (square[0] != None) and (square[0] == square[1]) and (square[1] == square[2]):
-        print("Congratulations! You have won. First row horizontal")
+        board()
+        print ('\n' + '\033[1;33mCongratulations! You have won! -> First row horizontal\033[0;0m')
+        print("Game Over!" + "\n")
+        exit()
     if (square[3] != None) and (square[3] == square[4]) and (square[4] == square[5]):
-        print("Congratulations! You have won. Middle row horizontal")
+        board()
+        print ('\n' + '\033[1;33mCongratulations! You have won! -> Middle row horizontal\033[0;0m')
+        print("Game Over!" + "\n")
+        exit()
     if (square[6] != None) and (square[6] == square[7]) and (square[7] == square[8]):
-        print("Congratulations! You have won. Last row horizontal")
+        board()
+        print ('\n' + '\033[1;33mCongratulations! You have won! -> Last row horizontal\033[0;0m')
+        print("Game Over!" + "\n")
+        exit()
     if (square[0] != None) and (square[0] == square[3]) and (square[3] == square[6]):
-        print("Congratulations! You have won. First row vertical")
+        board()
+        print ('\n' + '\033[1;33mCongratulations! You have won! -> First row vertical\033[0;0m')
+        print("Game Over!" + "\n")
+        exit()
     if (square[1] != None) and (square[1] == square[4]) and (square[4] == square[7]):
-        print("Congratulations! You have won. Middle row vertical")
+        board()
+        print ('\n' + '\033[1;33mCongratulations! You have won! -> Middle row vertical\033[0;0m')
+        print("Game Over!" + "\n")
+        exit()
     if (square[2] != None) and (square[2] == square[5]) and (square[5] == square[8]):
-        print("YCongratulations! You have won. Last row vertical")
+        board()
+        print ('\n' + '\033[1;33mCongratulations! You have won! -> Last row vertical\033[0;0m')
+        print("Game Over!" + "\n")
+        exit()
     if (square[0] != None) and (square[0] == square[4]) and (square[4] == square[8]):
-        print("Congratulations! You have won. Left diagonal")
+        board()
+        print ('\n' + '\033[1;33mCongratulations! You have won! -> Left diagonal\033[0;0m')
+        print("Game Over!" + "\n")
+        exit()
     if (square[2] != None) and (square[2] == square[4]) and (square[4] == square[6]):
-        print("Congratulations! You have won. Right diagonal")
-
-
-# def endGame():
-#     if (square[0] != None) and (square[0] == square[1]) and (square[1] == square[2]);
-
-#     if (square[2] == square[3]) and (square[3] == square[4]) and (square[4] == square[5]) and (square[5] == square[6]) and (square[6] == square[7]) and (square[7] == square[8]):
-#         print("Game Over")
-addX()
-addO()
-addX()
-addO()
-addX()
-addO()
-addX()
-addO()
-addX()
-winGame()
-
-
-
-# clear()
-# list.clear()
-
-
-
-
-
-
+        board()
+        print ('\n' + '\033[1;33mCongratulations! You have won! -> Right diagonal\033[0;0m')
+        print("Game Over!" + "\n")
+        exit()
 
 
 
@@ -130,7 +112,4 @@ winGame()
 # If yes, restart game
 # If no, quit game
 # Keep displaying new board when player's turn
-
-
-
 
